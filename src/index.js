c7dff644f4d1e43faf0d762031355c14bb57cb5d
George Matos <georgemattos@gmail.com>
2020-11-13 05:27:41 -0300

    ajustes no projeto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render( // used only one time in all application to render your main component
   <React.StrictMode>
-    <App />
+    <App /> {/* main component */}
   </React.StrictMode>,
   document.getElementById('root')
 );
