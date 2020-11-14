0561a3dbbba391d26dc2a29eb6b0b44bb6059caf
George Matos <georgemattos@gmail.com>
2020-11-14 08:41:08 -0300

    ajustes no component main e melhorando a apresentacao dos itens da lista

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import './styles.css';
 
+// statefull component
 class Main extends Component {
 
   state = {
@@ -21,10 +23,20 @@ class Main extends Component {
   }
 
   render() {
+    /* desestruturacao - declara um ou mais atributos e atribui
+    a ele/s o valor do atributo de mesmo nome do objeto declarado
+    a direita. */
+    const { products } = this.state;
+
     return (
       <div className="product-list">
-        {this.state.products.map(product => (
-          <h2 key={product._id}>{product.title}</h2>
+        {products.map(product => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+
+            <a href={product.url}>Acessar</a>
+          </article>
         ))}
       </div>
     )
